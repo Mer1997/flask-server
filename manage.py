4ff43ee6569d746c542a41d86b0c17b8ba7dbcb0
mer <919747016@qq.com>
2018-11-27 11:42:04 +0800

    add migration

--- manage.py
+++ manage.py
@@ -1,9 +1,13 @@
 from flask_script import Manager, Server
+from flask_migrate import Migrate,MigrateCommand
+
 from main import app, db, User, Log
 
-manager = Manager(app)
+migrate = Migrate(app,db)
 
+manager = Manager(app)
 manager.add_command("server", Server())
+manager.add_command('db',MigrateCommand)
 
 @manager.shell
 def make_shell_context():
